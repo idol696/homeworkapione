ddeb2a91c4f974bfc109bcbdc6288559d15f526e
Roman Kuzmin <ghelatin@mail.ru>
2024-08-11 18:07:35 +0300

    commit init

---
+++ src/main/java/ru/prostostudia/homeworkapione/HomeworkapioneApplication.java
@@ -0,0 +1,13 @@
+package ru.prostostudia.homeworkapione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkapioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkapioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/prostostudia/homeworkapione/HomeworkapioneApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prostostudia.homeworkapione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkapioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
